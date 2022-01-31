bc003fc0ce24e1f43a96583d735446baf6e2d103
Michael Corey <corey101@umn.edu>
2022-01-31 15:33:49 -0600

    Loading of dropdown reducer output

--- zoon/management/commands/load_zooniverse_export.py
+++ zoon/management/commands/load_zooniverse_export.py
@@ -27,52 +27,19 @@ class Command(BaseCommand):
         return json.dumps([int(u) for u in json.loads(input.replace('nan', '-1'))])
 
     def jsonify(self, input):
-        '''json.loads doesn't play nice with the output from Zooniverse here, possibly because of single quotes. Those are hard to replace without killing apostrophes, so we're using literal_eval'''
+        '''json.loads doesn't play nice with the output from Zooniverse from TEXT reducers, possibly because of single quotes. Those are hard to replace without killing apostrophes, so we're using literal_eval'''
         return json.dumps(ast.literal_eval(input))
 
+    def join_answers(self, row):
+        '''For dropdown reductions. Get rid of hashes for choices, and restructure slightly so the results can be sorted in descending order by number of votes for that answer'''
 
-    def load_texts_reduced(self, batch_dir: str, master_config: dict):
-        df = pd.read_csv(os.path.join(batch_dir, 'text_reducer_texts.csv'))
-        config_df = pd.DataFrame(master_config)
+        values = json.loads(row['data.value'].replace("'", '"'))
+        joined = []
+        for v in values:
+            for key, value in v.items():
+                joined.append({'choice': row['options'][key], 'votes': value})
 
-        # We're not really doing anything with the config data for text-type questions, but just to maintain parallel structure...
-        df = df.merge(
-            config_df,
-            how="left",
-            left_on="task",
-            right_on="task_num"
-        )
-
-        df.columns = df.columns.str.replace("data.", "", regex=False)
-
-        # Drop all rows from df with no text input.
-        df = df.dropna(subset=['aligned_text', 'consensus_text'], how='all')
-
-        # Parse user_ids as int to drop weirdo .zero
-        df['user_ids'] = df['user_ids'].apply(lambda x: self.round_user_ids(x))
-        df['aligned_text'] = df['aligned_text'].apply(lambda x: self.jsonify(x))
-
-        df = df.rename(columns={
-            'subject_id': 'zoon_subject_id',
-            'workflow_id': 'zoon_workflow_id',
-            'task': 'task_id',
-            'number_views': 'total_votes',
-        })[[
-            'zoon_subject_id',
-            'zoon_workflow_id',
-            'task_id',
-            'aligned_text',
-            'total_votes',
-            'consensus_text',
-            'consensus_score',
-            'user_ids',
-        ]]
-
-        print(df)
-
-        print('Sending reducer TEXT results to Django ...')
-        sa_engine = create_engine(settings.SQL_ALCHEMY_DB_CONNECTION_URL)
-        df.to_sql('zoon_reducedresponse_text', if_exists='append', index=False, con=sa_engine)
+        return sorted(joined, key = lambda i: i['votes'], reverse=True)
 
     def load_questions_reduced(self, batch_dir: str, master_config: dict):
         '''Process reduced responses from the question reducer
@@ -130,6 +97,7 @@ class Command(BaseCommand):
             'total_votes',
             'answer_scores',
         ]]
+        df['question_type'] = 'q'
 
         print(df)
 
@@ -137,6 +105,98 @@ class Command(BaseCommand):
         sa_engine = create_engine(settings.SQL_ALCHEMY_DB_CONNECTION_URL)
         df.to_sql('zoon_reducedresponse_question', if_exists='append', index=False, con=sa_engine)
 
+    def load_dropdowns_reduced(self, batch_dir: str, master_config: dict):
+        '''Process reduced responses from the dropdown reducer. In at least some versions, you need to look up hashes for fields.
+        Example: [{'adbad85a7b5ce': 1, '2b3caf88e1ee6': 2}] (In this case, 1 person chose the first, 2 people the second)
+
+        Arguments:
+            batch_dir: Path to the export files for this batch
+            master_config: Question text and label lookup object
+        '''
+
+        df = pd.read_csv(os.path.join(batch_dir, 'dropdown_reducer_dropdowns.csv'))
+        config_df = pd.DataFrame(master_config)
+
+        # Join responses to config so we know the possible answers to each question
+        df = df.merge(
+            config_df,
+            how="left",
+            left_on="task",
+            right_on="task_num"
+        )
+
+        # Drop all rows from df with no answers.
+        df = df.dropna(subset=['data.value'], how='all')
+
+        df['answer_scores'] = df.apply(lambda row: self.join_answers(row), axis=1)
+        df['best_answer'] = df['answer_scores'].apply(lambda x: x[0]['choice'])
+        df['best_answer_score'] = df['answer_scores'].apply(lambda x: x[0]['votes'])
+        df['total_votes'] = df['answer_scores'].apply(lambda x: sum([r['votes'] for r in x]))
+
+        df = df.rename(columns={
+            'subject_id': 'zoon_subject_id',
+            'workflow_id': 'zoon_workflow_id',
+            'task': 'task_id'
+        })[[
+            'zoon_subject_id',
+            'zoon_workflow_id',
+            'task_id',
+            'best_answer',
+            'best_answer_score',
+            'total_votes',
+            'answer_scores',
+        ]]
+        df['question_type'] = 'd'
+        df['answer_scores'] = df['answer_scores'].apply(lambda x: json.dumps(x))
+
+        print(df)
+
+        print('Sending reducer DROPDOWN results to Django ...')
+        sa_engine = create_engine(settings.SQL_ALCHEMY_DB_CONNECTION_URL)
+        df.to_sql('zoon_reducedresponse_question', if_exists='append', index=False, con=sa_engine)
+
+    def load_texts_reduced(self, batch_dir: str, master_config: dict):
+        df = pd.read_csv(os.path.join(batch_dir, 'text_reducer_texts.csv'))
+        config_df = pd.DataFrame(master_config)
+
+        # We're not really doing anything with the config data for text-type questions, but just to maintain parallel structure...
+        df = df.merge(
+            config_df,
+            how="left",
+            left_on="task",
+            right_on="task_num"
+        )
+
+        df.columns = df.columns.str.replace("data.", "", regex=False)
+
+        # Drop all rows from df with no text input.
+        df = df.dropna(subset=['aligned_text', 'consensus_text'], how='all')
+
+        # Parse user_ids as int to drop weirdo .zero
+        df['user_ids'] = df['user_ids'].apply(lambda x: self.round_user_ids(x))
+        df['aligned_text'] = df['aligned_text'].apply(lambda x: self.jsonify(x))
+
+        df = df.rename(columns={
+            'subject_id': 'zoon_subject_id',
+            'workflow_id': 'zoon_workflow_id',
+            'task': 'task_id',
+            'number_views': 'total_votes',
+        })[[
+            'zoon_subject_id',
+            'zoon_workflow_id',
+            'task_id',
+            'aligned_text',
+            'total_votes',
+            'consensus_text',
+            'consensus_score',
+            'user_ids',
+        ]]
+
+        print(df)
+
+        print('Sending reducer TEXT results to Django ...')
+        sa_engine = create_engine(settings.SQL_ALCHEMY_DB_CONNECTION_URL)
+        df.to_sql('zoon_reducedresponse_text', if_exists='append', index=False, con=sa_engine)
 
     def handle(self, *args, **kwargs):
         workflow_name = kwargs['workflow']
@@ -151,4 +211,5 @@ class Command(BaseCommand):
             master_config = parse_config_yaml(self.config_yaml)
 
             self.load_questions_reduced(self.batch_dir, master_config)
+            self.load_dropdowns_reduced(self.batch_dir, master_config)
             self.load_texts_reduced(self.batch_dir, master_config)
