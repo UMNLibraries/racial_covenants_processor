241dfbe6cf93139d13348a1673e04a7d5659f07a
Michael Corey <corey101@umn.edu>
2022-04-05 12:55:19 -0500

    Basic shapefile loading with uniform attributes

--- parcel/management/commands/load_parcel_shp.py
+++ parcel/management/commands/load_parcel_shp.py
@@ -1,10 +1,13 @@
 import re
 import os
 import glob
+import datetime
 import requests
 from zipfile import ZipFile
 
 from django.core.management.base import BaseCommand
+from django.contrib.gis.gdal import DataSource, OGRGeometry, OGRGeomType
+from django.contrib.gis.gdal.error import GDALException
 from django.conf import settings
 
 from parcel.models import Parcel
@@ -61,14 +64,14 @@ class Command(BaseCommand):
         except:
             file_prefix = False
 
-        with ZipFile(local_path, 'r') as zipObject:
+        with ZipFile(local_path, 'r') as zip_object:
 
             if file_prefix and file_prefix != '':
-                file_list = zipObject.namelist()
+                file_list = zip_object.namelist()
                 for member_file in file_list:
                     if re.match(rf"{shp_config['file_prefix']}\..+", member_file):
                         # Extract a single file from zip
-                        zipObject.extract(member_file, os.path.join(
+                        zip_object.extract(member_file, os.path.join(
                             self.shp_dir, shp_config['file_prefix']))
                 return os.path.join(self.shp_dir, shp_config['file_prefix'], f"{shp_config['file_prefix']}.shp")
 
@@ -78,6 +81,96 @@ class Command(BaseCommand):
                    # This part needs more refinement with a different data set
                    zip_obj.extractall(self.shp_dir)
                    return os.path.join(self.shp_dir, glob.glob('*.shp', recursive=True)[0])
+
+    def remove_z(self, wkt):
+        ''' Remove Z dimension from WKT. '''
+        z_search = r'([-\d\.]+) ([-\d\.]+) ([-\d\.]+)'
+        return re.sub(z_search, r'\1 \2', wkt)
+
+    def gather_all_attributes(self, field_list, feat):
+        ''' Go through each of the attributes in the shapefile, create a JSON object of them all, then put in a single kitchen-sink attribute on the matching Django instance. '''
+
+        data = {}
+        for field in field_list:
+            data[field] = feat.get(field)
+            if type(data[field]) in [datetime.date, datetime.datetime]:
+                try:
+                    data[field] = data[field].strftime('%Y-%m-%d')
+                except:
+                    data[field] = data[field].isoformat()
+
+        return data
+
+    def build_mapping(self, config):
+        ''' Construct a layermapping-ready mapping from the CSV'''
+        mapping = {}
+        for attr in ["pin_primary", "pin_secondary", "street_address", "city",
+                     "state", "zip_code", "county_name", "county_fips", "plat_name",
+                     "block", "lot", "join_description", "phys_description",
+                     "township", "range", "section"]:
+            if config[attr] != '':
+                mapping.update({attr: config[attr]})
+        return mapping
+
+    def save_parcels(self, workflow, shp_path, shp_mapping):
+        parcels = []
+        parcel_count = 0
+
+        ds = DataSource(shp_path)
+        layer = ds[0]
+
+        for feat in layer:
+            try:
+
+                # Freaky 3D geometries -- this seems harder than it should be.
+                if layer.geom_type == 'POLYGON25D':
+                    safe_geom = OGRGeometry(
+                        self.remove_z(feat.geom.wkt), layer.srs)
+                else:
+                    safe_geom = feat.geom
+
+                # force to multi
+                # print(layer.srs)
+                multipoly = OGRGeometry(OGRGeomType('MultiPolygon'), layer.srs)
+                multipoly.add(safe_geom)
+
+                # Translate to 4326, even if it probably already is.
+                multipoly_4326 = multipoly.transform(4326, clone=True)
+                # multipoly_utm = multipoly.transform(26915, clone=True)
+
+                all_attributes = self.gather_all_attributes(layer.fields, feat)
+
+                mapping = self.build_mapping(shp_mapping)
+                kwargs = {}
+                for k, v in mapping.items():
+                    # Check for static values
+                    if type(v) is tuple:
+                        kwargs[k] = v[1]
+                    else:
+                        kwargs[k] = all_attributes[v]
+                # kwargs = {k: all_attributes[v] for k, v in mapping.items()}
+
+                kwargs.update({
+                    'workflow_id': workflow.id,
+                    'feature_id': feat.fid,
+                    'orig_data': all_attributes,
+                    'geom_4326': multipoly_4326.wkt,
+                    # 'geom_utm': multipoly_utm.wkt,
+                })
+
+                parcels.append(Parcel(**kwargs))
+                parcel_count += 1
+
+                if parcel_count % 5000 == 0:
+                    Parcel.objects.bulk_create(parcels)
+                    print('Saved {} records...'.format(parcel_count))
+                    parcels = []
+
+            except GDALException as e:
+                pass
+
+        Parcel.objects.bulk_create(parcels)
+        print('Saved {} records...'.format(parcel_count))
 
     def handle(self, *args, **kwargs):
         workflow_name = kwargs['workflow']
@@ -102,3 +195,16 @@ class Command(BaseCommand):
             for shp in self.batch_config['parcel_shps']:
                 local_shp = self.download_shp(workflow, shp)
                 print(local_shp)
+
+                # city_obj, city_created = City.objects.update_or_create(
+                #     name=city,
+                #     defaults={
+                #         'orig_fields': ','.join(layer.fields),
+                #         'orig_file': shp_path,
+                #         'met_council_type': c['MetCouncilType'],
+                #     },
+                # )
+
+                print(
+                    'Beginning homegrown layermapping: {} ...'.format(local_shp))
+                self.save_parcels(workflow, local_shp, shp['mapping'])
