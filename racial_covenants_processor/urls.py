30ccff8a2d799581e36e514aaa470f2469946956
Michael Corey <corey101@umn.edu>
2022-04-14 15:31:03 -0500

    Added very basic workflow summary template

--- racial_covenants_processor/urls.py
+++ racial_covenants_processor/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.zoon import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('workflow/<int:workflow_id>/', views.workflow_summary, name='workflow'),
 ]
