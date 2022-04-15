656d3d57360dc7c496cc76567467540615e6efce
Michael Corey <corey101@umn.edu>
2022-04-14 19:23:58 -0500

    removing index for plat id

--- apps/plat/models.py
+++ apps/plat/models.py
@@ -8,10 +8,11 @@ from racial_covenants_processor.storage_backends import PrivateMediaStorage
 class Plat(models.Model):
     workflow = models.ForeignKey(ZooniverseWorkflow, on_delete=models.SET_NULL)
     plat_name = models.CharField(max_length=255, db_index=True, null=True)
+    plat_year = models.IntegerField(null=True, blank=True)
     book_name = models.CharField(
         max_length=255, db_index=True, null=True, blank=True)
     gov_id = models.CharField(
-        max_length=255, db_index=True, null=True, blank=True)
+        max_length=255, null=True, blank=True)  # Can't be an index because some plat maps have multiple plats on same map page. Maybe revisit this later
 
     def slug(self):
         return slugify(f"{self.workflow.slug} {self.name}")
@@ -23,6 +24,7 @@ class Plat(models.Model):
 class PlatMapPage(models.Model):
     plat = models.ForeignKey(Plat, on_delete=models.CASCADE)
     page_num = models.IntegerField(null=True)
+    remote_link = models.URLField(blank=True)
     page_image_web = models.ImageField(
         storage=PrivateMediaStorage(), null=True)
 
