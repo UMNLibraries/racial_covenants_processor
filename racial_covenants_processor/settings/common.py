dde58048646d67bfb05e6cb6656d112982412787
Michael Corey <corey101@umn.edu>
2022-04-20 15:57:14 -0500

    alternate DB setting for Github CI

--- racial_covenants_processor/settings/common.py
+++ racial_covenants_processor/settings/common.py
@@ -94,6 +94,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.contrib.gis.db.backends.postgis',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
