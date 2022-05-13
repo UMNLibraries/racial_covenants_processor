89503d0e4d03e4d7d6ed5abfd704d5a032cdfa54
Michael Corey <corey101@umn.edu>
2022-05-13 15:11:27 -0500

    Closes #39

--- apps/parcel/admin.py
+++ apps/parcel/admin.py
@@ -5,7 +5,29 @@ from .models import Parcel
 
 @admin.register(Parcel)
 class ParcelAdmin(admin.ModelAdmin):
-    exclude = ['geom_4326']
+    fields = [
+        'workflow',
+        'feature_id',
+        'join_strings',
+        'pin_primary',
+        'pin_secondary',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+        'county_name',
+        'county_fips',
+        'plat_name',
+        'plat_standardized',
+        'block',
+        'lot',
+        'join_description',
+        'phys_description',
+        'township',
+        'range',
+        'section',
+        'orig_filename',
+    ]
     list_filter = ['workflow', 'city']
     list_display = ['pin_primary', 'street_address', 'plat', 'city', 'plat_name', 'block', 'lot']
     search_fields = ['plat_name', 'pin_primary', 'street_address', 'city']
@@ -13,4 +35,10 @@ class ParcelAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         '''Make all fields read-only'''
-        return [f.name for f in self.model._meta.fields]
+        return [f.name for f in self.model._meta.fields] + ['join_strings']
+
+    # def join_strings(self, obj):
+    #     return obj.join_strings
+    #
+    # join_strings.short_description = 'Join strings'
+    # # join_strings.allow_tags = True
