b8286fe850cb34f87888a470afb5dea8b29f090d
Michael Corey <corey101@umn.edu>
2022-07-14 19:13:12 -0500

    Finished gathering deed hit info

--- apps/deed/models.py
+++ apps/deed/models.py
@@ -6,9 +6,17 @@ from racial_covenants_processor.storage_backends import PrivateMediaStorage
 from apps.zoon.models import ZooniverseWorkflow, ZooniverseSubject
 
 
+class MatchTerm(models.Model):
+    term = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.term
+
+
 class DeedPage(models.Model):
     workflow = models.ForeignKey(
         ZooniverseWorkflow, on_delete=models.CASCADE, null=True)
+    s3_lookup = models.CharField(blank=True, max_length=500, db_index=True)
     doc_num = models.CharField(blank=True, max_length=100)
     page_num = models.IntegerField(null=True)
     doc_date = models.DateField(null=True)
@@ -18,7 +26,7 @@ class DeedPage(models.Model):
     page_ocr_text = models.FileField(
         storage=PrivateMediaStorage(), null=True)
     bool_match = models.BooleanField(default=False)
-    matched_terms = models.JSONField(null=True)
+    matched_terms = models.ManyToManyField(MatchTerm)
 
     zooniverse_subject = models.ForeignKey(
         ZooniverseSubject, on_delete=models.SET_NULL, null=True)
@@ -28,6 +36,13 @@ class DeedPage(models.Model):
         if self.page_image_web:
             return mark_safe(f'<a href="{self.page_image_web.url}" target="_blank"><img src="{self.page_image_web.url}" width="100" /></a>')
         return ""
+
+    # Need to save through private media storage in gather_deed_images.py
+    # @property
+    # def page_ocr_text_link(self):
+    #     if self.page_image_web:
+    #         return mark_safe(f'<a href="{self.page_image_web.url}" target="_blank"><img src="{self.page_image_web.url}" width="100" /></a>')
+    #     return ""
 
 
 class SearchHitReport(models.Model):
