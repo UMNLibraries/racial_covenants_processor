06e1eaf9a428d0a1a776e255861c45cb1cfae83f
Michael Corey <corey101@umn.edu>
2023-02-23 11:36:40 -0600

    Fixed test case

--- apps/plat/tests.py
+++ apps/plat/tests.py
@@ -10,11 +10,12 @@ class PlatTests(TestCase):
     def test_standardize_addition(self):
         """Does standardize_addition give expected output?"""
         s = Plat.objects.get(plat_name="F. AMB'S ADDITION TO WEST ST. PAUL")
-        self.assertEquals(standardize_addition(s.plat_name), "f. ambs")
+        self.assertEquals(standardize_addition(s.plat_name), "f ambs")
 
     def test_plat_name_standardized(self):
         """Does plat_name_standardized match output of standardize_addition?"""
         s = Plat.objects.get(plat_name="F. AMB'S ADDITION TO WEST ST. PAUL")
+        print(standardize_addition(s.plat_name))
         self.assertEquals(s.plat_name_standardized,
                           standardize_addition(s.plat_name))
 
