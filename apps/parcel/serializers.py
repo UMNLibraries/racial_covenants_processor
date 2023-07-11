63288dfdae77d9f4e3d57c86ac2ae387376748da
Michael Corey <corey101@umn.edu>
2023-07-11 13:55:40 -0500

    Trying in-memory caching of API views

--- apps/parcel/serializers.py
+++ apps/parcel/serializers.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import serializers, viewsets
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
@@ -154,11 +156,19 @@ class CovenantNoGeoViewSet(viewsets.ModelViewSet):
     serializer_class = ParcelNoGeoSerializer
     filterset_class = CovenantFilter
 
+    @method_decorator(cache_page(60*30*1))
+    def list(self, *args, **kwargs):
+        return super().list(*args, **kwargs)
+
 
 class CovenantGeoViewSet(viewsets.ModelViewSet):
     queryset = Parcel.covenant_objects.all()
     serializer_class = ParcelGeoSerializer
     filterset_class = CovenantFilter
+
+    @method_decorator(cache_page(60*30*1))
+    def list(self, *args, **kwargs):
+        return super().list(*args, **kwargs)
 
 
 class ShpExportViewSet(viewsets.ModelViewSet):
