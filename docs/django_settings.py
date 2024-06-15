5e2b30279b963511fabd6175931ed8601113989f
Michael Corey <corey101@umn.edu>
2024-06-14 20:03:09 -0500

    Trying minimal Django settings for hopefully fewer docs requirements

---
+++ docs/django_settings.py
@@ -0,0 +1,24 @@
+"""
+Minimal file so Sphinx can work with Django for autodocumenting.
+
+Location: /docs/django_settings.py
+"""
+
+# SECRET_KEY for the documentation
+SECRET_KEY = 'docs-super-secret'
+
+# INSTALLED_APPS with these apps is necessary for Sphinx to build without warnings & errors
+# Depending on your package, the list of apps may be different
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # custom apps:
+    "apps.deed",
+    "apps.parcel",
+    "apps.plat",
+    "apps.zoon",
+]
