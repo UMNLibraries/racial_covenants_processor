eaf7008bca6e86a39ffa09b984a78e83a3ab9430
Michael Corey <corey101@umn.edu>
2024-07-03 14:35:18 -0500

    handling null zooniverse_subject on manualcorrection delete

--- apps/zoon/models.py
+++ apps/zoon/models.py
@@ -591,14 +591,14 @@ class ExtraParcelCandidate(models.Model):
 
 @receiver(models.signals.post_delete, sender=ManualCorrection)
 def model_delete(sender, instance, **kwargs):
-    if instance != None:
+    if instance is not None:
         instance.zooniverse_subject.get_final_values()
         instance.zooniverse_subject.save()
 
 
 @receiver(models.signals.post_delete, sender=ExtraParcelCandidate)
 def model_delete(sender, instance, **kwargs):
-    if instance != None:
+    if instance is not None:
         instance.zooniverse_subject.get_final_values()
         instance.zooniverse_subject.save()
 
