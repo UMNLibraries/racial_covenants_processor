faf382a2b6a281b5bfc8e6583e18844b575a553d
Michael Corey <corey101@umn.edu>
2025-06-25 15:31:29 -0500

    fixed bug with exporting all_covenanted_docs

--- apps/zoon/utils/django_export.py
+++ apps/zoon/utils/django_export.py
@@ -1,4 +1,5 @@
 import os
+import json
 import datetime
 import pandas as pd
 
@@ -34,9 +35,19 @@ def dump_cx_model_backups(workflow, app_name, model_name):
     ).values()
 
     df = pd.DataFrame(objs)
+    if df.shape[0] == 0:
+        print (f"No {model_name} records found in workflow {workflow_name}")
+        return False
     df.rename(columns={'id': 'db_id'}, inplace=True)
     df.drop(
         columns=['workflow_id', 'zooniverse_subject_id'], inplace=True, errors='ignore')
+    
+    print(df.columns)
+    if model_name == 'ZooniverseSubject':
+        df['image_ids'] = df['image_ids'].apply(lambda x: json.dumps(x))
+        df['image_links'] = df['image_links'].apply(lambda x: json.dumps(x))
+        df['join_candidates'] = df['join_candidates'].apply(lambda x: json.dumps(x))
+        df['parcel_addresses'] = df['parcel_addresses'].apply(lambda x: json.dumps(x))
 
     print(df)
     outfile = save_backup_file(df, workflow_name, model_name.lower())
@@ -64,3 +75,20 @@ def dump_individual_response_model_backups(workflow):
     outfile = save_backup_file(df, workflow_name, 'zooniverseresponseprocessed')
 
     return outfile
+
+
+def check_workflow_match(workflow, infile_path):
+    '''This function checks to see if the workflow in the CSV matches the specified workflow from the user command'''
+    
+    df = pd.read_csv(infile_path)
+    workflow_names = df.workflow_name.drop_duplicates().to_list()
+    if len(workflow_names) > 1:
+        print("Hmm, there is more than 1 workflow name in this import file. Exiting.")
+        return False
+    
+    if workflow.workflow_name == workflow_names[0]:
+        print("Workflow name in CSV matches selected workflow.")
+        return True
+    else:
+        print("Workflow name in CSV DOES NOT match selected workflow. Exiting. (NOTE: If you would like to migrate subjects to a new workflow, you can manually replace values in this CSV with values from the new workflow and try importing again.)")
+        return False
