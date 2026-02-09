a07ffb4ee1496d26aebe95a5ebd7c973cf049430
Katlyn Alo <katlyn@computer.local>
2026-02-08 21:42:55 -0700

    deed page search form for related view

--- apps/deed/forms.py
+++ apps/deed/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from apps.zoon.models import ZooniverseWorkflow
 
 class DeedSearchForm(forms.Form):
     q = forms.CharField(
@@ -12,10 +13,18 @@ class DeedSearchForm(forms.Form):
     workflow = forms.ChoiceField(
         label="Workflow filter",
         required=False,
-        choices=[("", "All workflows")],  # you can populate dynamically later
+        choices=[],  # Will be populated in __init__
     )
 
     bool_match = forms.BooleanField(
         label="Matches only?",
         required=False,
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Populate workflow choices from database
+        workflows = ZooniverseWorkflow.objects.all().order_by('workflow_name')
+        workflow_choices = [("", "All workflows")]
+        workflow_choices.extend([(str(w.id), w.workflow_name) for w in workflows])
+        self.fields['workflow'].choices = workflow_choices
